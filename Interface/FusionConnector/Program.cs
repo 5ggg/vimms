35a25783592e4f53e7e99462fc0c61c0f85a544a
Joe Wandy <joewandy@gmail.com>
2019-11-12 17:02:17 +0000

    Improved FusionConnector

--- Interface/FusionConnector/Program.cs
+++ Interface/FusionConnector/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Thermo.Interfaces.FusionAccess_V1;
 using Thermo.Interfaces.FusionAccess_V1.Control;
 using Thermo.Interfaces.FusionAccess_V1.MsScanContainer;
+using Thermo.Interfaces.InstrumentAccess_V1.Control;
 using Thermo.Interfaces.InstrumentAccess_V1.Control.Acquisition;
 using Thermo.Interfaces.InstrumentAccess_V1.Control.Acquisition.Modes;
 using Thermo.Interfaces.InstrumentAccess_V1.Control.Scans;
@@ -20,6 +21,7 @@ namespace FusionConnector
         private IFusionMsScanContainer ScanContainer { get; set; }
         private IFusionControl InstrumentControl { get; set; }
         private IScans ScanControl { get; set; }
+        private long runningNumber = 100000;    // start with an offset to make sure it's "us"
 
         static void Main(string[] args)
         {
@@ -32,14 +34,50 @@ namespace FusionConnector
                 IState state = e.State;
                 string msg = string.Format("System mode = {0}, system state = {1}", state.SystemMode,
                     state.SystemState);
-                Console.WriteLine(msg);
+                fusionBridge.WriteLog(msg);
             };
 
-            // Turn on the instrument and wait until it is On
-            fusionBridge.TurnOn();
+            // Register scan arrival event handler
+            fusionBridge.ScanContainer.MsScanArrived += (object sender, MsScanEventArgs e) =>
+            {
+                IMsScan scan = e.GetScan();
+                if (scan == null)
+                {
+                    fusionBridge.WriteLog(string.Format("[{0:HH:mm:ss.ffff}] Empty scan", DateTime.Now));
+                }
+                else
+                {
+                    fusionBridge.WriteLog(string.Format("[{0:HH:mm:ss.ffff}] Received MS Scan Number {1} -- {2} peaks",
+                    DateTime.Now,
+                    scan.Header["Scan"],
+                    scan.CentroidCount));
+                }
+            };
 
-            // Turn off the instrument and wait until it is Off
-            fusionBridge.TurnOff();
+            // send the initial custom scan
+            double startMz = 524;
+            double endMz = 524.5;
+            double isolationWidth = 0.7;
+            double collisionEnergy = 35;
+
+            double precursorMass = startMz;
+            fusionBridge.CreateCustomScan(precursorMass, isolationWidth, collisionEnergy, 2);
+            fusionBridge.ScanControl.CanAcceptNextCustomScan += (object sender, EventArgs e) =>
+            {
+                if (precursorMass < endMz)
+                {
+                    precursorMass += 0.02;
+                    fusionBridge.CreateCustomScan(precursorMass, isolationWidth, collisionEnergy, 2);
+                }
+            };
+
+            // https://stackoverflow.com/questions/2555292/how-to-run-code-before-program-exit
+            // https://stackoverflow.com/questions/33060838/c-sharp-processexit-event-handler-not-triggering-code
+            AppDomain.CurrentDomain.ProcessExit += (sender, EventArgs) =>
+            {
+                fusionBridge.CloseDown();
+            };
+            Console.ReadLine();
 
         }
 
@@ -85,23 +123,90 @@ namespace FusionConnector
             WriteLog(msg);
         }
 
-        public void TurnOn()
+        public void DumpPossibleParameters()
         {
-            IMode mode = InstrumentControl.Acquisition.CreateOnMode();
-            InstrumentControl.Acquisition.SetMode(mode);
-            while (SystemStateToString(InstrumentControl.Acquisition.State) != "On")
+            WriteLog("DumpPossibleParameters", true);
+            IParameterDescription[] parameters = ScanControl.PossibleParameters;
+            if (parameters.Length == 0)
             {
-                ;
+                WriteLog("No possible IScans parameters known.", true);
+            }
+
+            WriteLog("ScanControl parameters:");
+            foreach (IParameterDescription parameter in parameters)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("   '{0}' ", parameter.Name);
+                if (parameter.Selection == "")
+                {
+                    sb.AppendFormat("doesn't accept an argument, help: {0}", parameter.Help);
+                }
+                else
+                {
+                    sb.AppendFormat("accepts '{0}', default='{1}', help: {2}", parameter.Selection, parameter.DefaultValue, parameter.Help);
+                }
+                WriteLog(sb.ToString());
             }
         }
 
-        public void TurnOff()
+        public void CreateCustomScan(double precursorMass, double isolationWidth, double collisionEnergy, int msLevel,
+            string polarity="Positive", double firstMass=50, double lastMass=600, double singleProcessingDelay=0.50D)
         {
-            IMode mode = InstrumentControl.Acquisition.CreateOffMode();
-            InstrumentControl.Acquisition.SetMode(mode);
-            while (SystemStateToString(InstrumentControl.Acquisition.State) != "Off")
+            WriteLog("StartNewScan called", true);
+            if (ScanControl.PossibleParameters.Length > 0)
             {
-                ;
+                ICustomScan cs = ScanControl.CreateCustomScan();
+                cs.RunningNumber = runningNumber++;
+
+                // Allow an extra delay of 500 ms, we will answer as fast as possible, so this is a maximum value.
+                cs.SingleProcessingDelay = singleProcessingDelay;
+
+                // Set the custom scan parameters
+                cs.Values["CollisionEnergy"] = collisionEnergy.ToString();
+                cs.Values["FirstMass"] = firstMass.ToString();
+                cs.Values["LastMass"] = lastMass.ToString();
+                cs.Values["Analyzer"] = "Orbitrap";
+                cs.Values["Polarity"] = polarity;
+                if (msLevel == 1)
+                {
+                    cs.Values["ScanType"] = "Full";
+                    cs.Values["IsolationWidth"] = "0.7"; // default
+                    cs.Values["PrecursorMass"] = ""; // default
+                }
+                else
+                {
+                    cs.Values["ScanType"] = "MSn";
+                    cs.Values["IsolationWidth"] = isolationWidth.ToString();
+                    cs.Values["PrecursorMass"] = precursorMass.ToString();
+                }
+                cs.Values["DataType"] = "Centroid";
+
+                // Dump key-value pairs in cs.Values
+                foreach (KeyValuePair<string, string> kvp in cs.Values)
+                {
+                    string msg = string.Format("cs.Values Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+                    WriteLog(msg);
+                }
+
+                try
+                {
+                    if (!ScanControl.SetCustomScan(cs))
+                    {
+                        WriteLog("New custom scan has not been placed, connection to service broken!!", true);
+                    }
+                    else
+                    {
+                        WriteLog("Placed a new custom scan(" + cs.RunningNumber + ") for precursor mz=" + cs.Values["PrecursorMass"], true);
+                    }
+                }
+                catch (Exception e)
+                {
+                    WriteLog("Error placing a new scan: " + e.Message, true);
+                }
+            }
+            else
+            {
+                WriteLog("New custom scan has not been placed, no parameters available!!", true);
             }
         }
 
@@ -115,14 +220,18 @@ namespace FusionConnector
             }
         }
 
-        private string SystemModeToString(IState state)
+        private void CloseDown()
         {
-            return Enum.GetName(typeof(SystemMode), state.SystemMode);
-        }
+            WriteLog("Goodbye Cruel World", true);
 
-        private string SystemStateToString(IState state)
-        {
-            return Enum.GetName(typeof(InstrumentState), state.SystemState);
+            // write log files to Desktop
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = "FusionBridge_" + DateTime.Now.ToFileTime() + ".txt";
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, fileName)))
+            {
+                foreach (string line in Logs)
+                    outputFile.WriteLine(line);
+            }
         }
 
     }
