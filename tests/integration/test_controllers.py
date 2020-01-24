bd0d37d7bee9de711d8ff91ba84be87387911545
Joe Wandy <joewandy@gmail.com>
2020-01-24 13:19:26 +0000

    Added more unit tests

--- tests/integration/test_controllers.py
+++ tests/integration/test_controllers.py
@@ -24,17 +24,20 @@ max_rt = rt_range[0][1]
 mz_range = [(0, 1050)]
 n_chems = 500
 
+beer_chems = load_obj(Path(base_dir, 'QCB_22May19_1.p'))
+
 
 class TestMS1Controller(unittest.TestCase):
     """
     Tests the MS1 controller that does MS1 full-scans only with the simulated mass spec class.
     """
+
     def setUp(self):
         self.ps = load_obj(Path(base_dir, 'peak_sampler_mz_rt_int_beerqcb_fullscan.p'))
         self.ms_level = 1
 
-    def test_ms1_controller_can_run(self):
-        logger.info('Testing MS1 Controller')
+    def test_ms1_controller_with_simulated_chems(self):
+        logger.info('Testing MS1 controller with simulated chemicals')
 
         # create some chemical objects
         chems = ChemicalCreator(self.ps, ROI_Sources, hmdb)
@@ -58,7 +61,33 @@ class TestMS1Controller(unittest.TestCase):
         set_log_level_debug()
 
         # write simulated output to mzML file
-        filename = 'multibeer_ms1.mzML'
+        filename = 'ms1_controller_simulated_chems.mzML'
+        out_file = os.path.join(out_dir, filename)
+        env.write_mzML(out_dir, filename)
+        self.assertTrue(os.path.exists(out_file))
+        print()
+
+    def test_ms1_controller_with_qcbeer_chems(self):
+        logger.info('Testing MS1 controller with QC beer chemicals')
+
+        # create a simulated mass spec and MS1 controller
+        mass_spec = IndependentMassSpectrometer(POSITIVE, beer_chems, self.ps)
+        controller = SimpleMs1Controller()
+
+        # create an environment to run both the mass spec and controller
+        env = Environment(mass_spec, controller, min_rt, max_rt, progress_bar=True)
+
+        # set the log level to WARNING so we don't see too many messages when environment is running
+        set_log_level_warning()
+
+        # run the simulation
+        env.run()
+
+        # set the log level back to DEBUG
+        set_log_level_debug()
+
+        # write simulated output to mzML file
+        filename = 'ms1_controller_qcbeer_chems.mzML'
         out_file = os.path.join(out_dir, filename)
         env.write_mzML(out_dir, filename)
         self.assertTrue(os.path.exists(out_file))
@@ -74,8 +103,8 @@ class TestTopNController(unittest.TestCase):
         self.ps = load_obj(Path(base_dir, 'peak_sampler_mz_rt_int_beerqcb_fragmentation.p'))
         self.ms_level = 1
 
-    def test_TopN_controller_can_run(self):
-        logger.info('Testing Top-N Controller')
+    def test_TopN_controller_with_simulated_chems(self):
+        logger.info('Testing Top-N controller with simulated chemicals')
 
         # create some chemical objects
         chems = ChemicalCreator(self.ps, ROI_Sources, hmdb)
@@ -107,7 +136,7 @@ class TestTopNController(unittest.TestCase):
         set_log_level_debug()
 
         # write simulated output to mzML file
-        filename = 'multibeer_TopN_ms2Peaks_noNoise.mzML'
+        filename = 'topN_controller_simulated_chems_no_noise.mzML'
         out_file = os.path.join(out_dir, filename)
         env.write_mzML(out_dir, filename)
         self.assertTrue(os.path.exists(out_file))
@@ -130,7 +159,39 @@ class TestTopNController(unittest.TestCase):
         set_log_level_debug()
 
         # write simulated output to mzML file
-        filename = 'multibeer_TopN_ms2Peaks_withNoise.mzML'
+        filename = 'topN_controller_simulated_chems_with_noise.mzML'
+        out_file = os.path.join(out_dir, filename)
+        env.write_mzML(out_dir, filename)
+        self.assertTrue(os.path.exists(out_file))
+        print()
+
+    def test_TopN_controller_with_beer_chems(self):
+        logger.info('Testing Top-N controller with QC beer chemicals')
+
+        isolation_width = 1
+        N = 10
+        rt_tol = 15
+        mz_tol = 10
+        ionisation_mode = POSITIVE
+
+        # create a simulated mass spec without noise and Top-N controller
+        mass_spec = IndependentMassSpectrometer(ionisation_mode, beer_chems, self.ps, add_noise=False)
+        controller = TopNController(ionisation_mode, N, isolation_width, mz_tol, rt_tol, min_ms1_intensity)
+
+        # create an environment to run both the mass spec and controller
+        env = Environment(mass_spec, controller, min_rt, max_rt, progress_bar=True)
+
+        # set the log level to WARNING so we don't see too many messages when environment is running
+        set_log_level_warning()
+
+        # run the simulation
+        env.run()
+
+        # set the log level back to DEBUG
+        set_log_level_debug()
+
+        # write simulated output to mzML file
+        filename = 'topN_controller_qcbeer_chems_no_noise.mzML'
         out_file = os.path.join(out_dir, filename)
         env.write_mzML(out_dir, filename)
         self.assertTrue(os.path.exists(out_file))
@@ -149,8 +210,8 @@ class TestHybridController(unittest.TestCase):
         self.ps = load_obj(Path(base_dir, 'peak_sampler_mz_rt_int_beerqcb_fragmentation.p'))
         self.ms_level = 1
 
-    def test_hybrid_controller_can_run(self):
-        logger.info('Testing Hybrid Controller')
+    def test_hybrid_controller_with_simulated_chems(self):
+        logger.info('Testing hybrid controller with simulated chemicals')
 
         # create some chemical objects
         chems = ChemicalCreator(self.ps, ROI_Sources, hmdb)
@@ -163,7 +224,7 @@ class TestHybridController(unittest.TestCase):
         N = [5, 10, 15, 20]
         rt_tol = [15, 30, 60, 120]
         mz_tol = [10, 5, 15, 20]
-        scan_param_changepoints = [300, 600, 900] # the timepoints when we will change the 4 parameters above
+        scan_param_changepoints = [300, 600, 900]  # the timepoints when we will change the 4 parameters above
         ionisation_mode = POSITIVE
 
         # create a simulated mass spec with noise and Hybrid controller
@@ -184,7 +245,56 @@ class TestHybridController(unittest.TestCase):
         set_log_level_debug()
 
         # write simulated output to mzML file
-        filename = 'QCB_Hybrid_ms2Spectra.mzML'
+        filename = 'hybrid_controller_simulated_chems.mzML'
+        out_file = os.path.join(out_dir, filename)
+        env.write_mzML(out_dir, filename)
+        self.assertTrue(os.path.exists(out_file))
+        print()
+
+    def test_hybrid_controller_with_beer_chems(self):
+        logger.info('Testing hybrid controller with QC beer chemicals')
+
+        isolation_window = [1]  # the isolation window in Dalton around a selected precursor ion
+        N = [5]
+        rt_tol = [10]
+        mz_tol = [10]
+        min_ms1_intensity = 1.75E5
+        scan_param_changepoints = None
+        rt_range = [(0, 400)]
+        min_rt = rt_range[0][0]
+        max_rt = rt_range[0][1]
+        n_purity_scans = N[0]
+        purity_shift = 0.2
+        purity_threshold = 1
+
+        # these settings change the Mass Spec type. They arent necessary to run the Top-N ROI Controller
+        isolation_transition_window = 'gaussian'
+        isolation_transition_window_params = [0.5]
+
+        purity_add_ms1 = True  # this seems to be the broken bit
+        purity_randomise = True
+
+        mass_spec = IndependentMassSpectrometer(POSITIVE, beer_chems, self.ps, add_noise=True,
+                                                isolation_transition_window=isolation_transition_window,
+                                                isolation_transition_window_params=isolation_transition_window_params)
+        controller = HybridController(mass_spec, N, scan_param_changepoints, isolation_window, mz_tol, rt_tol,
+                                      min_ms1_intensity, n_purity_scans, purity_shift, purity_threshold,
+                                      purity_add_ms1=purity_add_ms1, purity_randomise=purity_randomise)
+
+        # create an environment to run both the mass spec and controller
+        env = Environment(mass_spec, controller, min_rt, max_rt, progress_bar=True)
+
+        # set the log level to WARNING so we don't see too many messages when environment is running
+        set_log_level_warning()
+
+        # run the simulation
+        env.run()
+
+        # set the log level back to DEBUG
+        set_log_level_debug()
+
+        # write simulated output to mzML file
+        filename = 'hybrid_controller_qcbeer_chems.mzML'
         out_file = os.path.join(out_dir, filename)
         env.write_mzML(out_dir, filename)
         self.assertTrue(os.path.exists(out_file))
@@ -196,12 +306,13 @@ class TestROIController(unittest.TestCase):
     Tests the ROI controller that performs fragmentations and dynamic exclusions based on selecting regions of interests
     (rather than the top-N most intense peaks)
     """
+
     def setUp(self):
         self.ps = load_obj(Path(base_dir, 'peak_sampler_mz_rt_int_beerqcb_fragmentation.p'))
         self.ms_level = 1
 
-    def test_roi_controller_can_run(self):
-        logger.info('Testing ROI Controller')
+    def test_roi_controller_with_simulated_chems(self):
+        logger.info('Testing ROI controller with simulated chemicals')
 
         # create some chemical objects
         chems = ChemicalCreator(self.ps, ROI_Sources, hmdb)
@@ -235,7 +346,42 @@ class TestROIController(unittest.TestCase):
         set_log_level_debug()
 
         # write simulated output to mzML file
-        filename = 'QCB_ROIController.mzML'
+        filename = 'roi_controller_simulated_chems.mzML'
+        out_file = os.path.join(out_dir, filename)
+        env.write_mzML(out_dir, filename)
+        self.assertTrue(os.path.exists(out_file))
+        print()
+
+    def test_roi_controller_with_beer_chems(self):
+        logger.info('Testing ROI controller with QC beer chemicals')
+
+        isolation_width = 1  # the isolation window in Dalton around a selected precursor ion
+        N = 10
+        rt_tol = 15
+        mz_tol = 10
+        min_roi_intensity = 5000
+        min_roi_length = 10
+        ionisation_mode = POSITIVE
+
+        # create a simulated mass spec with noise and ROI controller
+        mass_spec = IndependentMassSpectrometer(ionisation_mode, beer_chems, self.ps, add_noise=True)
+        controller = RoiController(ionisation_mode, isolation_width, mz_tol, min_ms1_intensity,
+                                   min_roi_intensity, min_roi_length, "Top N", N, rt_tol)
+
+        # create an environment to run both the mass spec and controller
+        env = Environment(mass_spec, controller, min_rt, max_rt, progress_bar=True)
+
+        # set the log level to WARNING so we don't see too many messages when environment is running
+        set_log_level_warning()
+
+        # run the simulation
+        env.run()
+
+        # set the log level back to DEBUG
+        set_log_level_debug()
+
+        # write simulated output to mzML file
+        filename = 'roi_controller_qcbeer_chems.mzML'
         out_file = os.path.join(out_dir, filename)
         env.write_mzML(out_dir, filename)
         self.assertTrue(os.path.exists(out_file))
